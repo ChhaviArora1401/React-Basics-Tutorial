e8c5cad9a1082f5e4f703d8cf982b6c16a90e4ac
Chhavi Arora <chhaviarora1401@gmail.com>
2021-12-02 22:19:10 +0530

    Emmets and Extentions

---
+++ src/testing.js
@@ -0,0 +1,14 @@
+//look for ES7 React/Redux Extention commands
+// rafce
+// ==>
+import React from 'react'
+
+const testing = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default testing
